ae22a3c1e3a424cf1af867335b0ee0211828b29c
pavithran1999 <pavithranr65@gmail.com>
2023-02-18 23:53:45 +0530

    Started the project

---
+++ src/components/Navbar/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classNames from "../../styles/components/navbar.module.scss";
+
+const Navbar = () => {
+  return <div className={classNames.navbar}></div>;
+};
+
+export default Navbar;
