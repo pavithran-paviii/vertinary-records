abb5399408a0af2b2302e31534a5cd8002e04889
pavithran1999 <pavithranr65@gmail.com>
2023-02-19 23:19:56 +0530

    Initiated login page and signup page

---
+++ src/pages/Login/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classNames from "../../styles/pages/login.module.scss";
+
+const Login = () => {
+  return <div className={classNames.login}>Login</div>;
+};
+
+export default Login;
---
+++ src/pages/Signup/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import classNames from "../../styles/pages/signup.module.scss";
+
+const Signup = () => {
+  return <div className={classNames.signup}>Signup</div>;
+};
+
+export default Signup;
