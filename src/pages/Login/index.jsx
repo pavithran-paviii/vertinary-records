ba238e3698910a0ad9872c5265deab1818320df3
pavithran1999 <pavithranr65@gmail.com>
2023-02-20 23:48:22 +0530

    Just some commits

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import classNames from "../../styles/pages/login.module.scss";
 
 const Login = () => {
-  return <div className={classNames.login}>Login</div>;
+  return <div className={classNames.login}></div>;
 };
 
 export default Login;
